e6fc2b8b11ffd9364f29c6bc3f7e5ab1fd69e13b
Nitin <sharmanitan938@gmail.com>
2025-03-13 21:35:56 +0530

    Making home page

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config()
+}
+// console.log(process.env.ATLASDB_URL)
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const ejsMate = require("ejs-mate");
-
+const Client = require("./models/client.js")
 
 app.set("view engine" , "ejs");// for views folder
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));// for public folder
+app.use(express.urlencoded({extended : true}));
 
-
+// let dbUrl = process.env.ATLASDB_URL;
 main().then(() => {console.log("Connected to DB")}).catch(err => console.log(err));
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/shreeshyam');
@@ -19,24 +25,33 @@ async function main() {
 app.get("/" , (req,res) => {
     res.render("home.ejs");
 })
-app.get("/about-us", (req,res) => {
-    res.render("aboutUs");
+app.get("/register" , (req,res) => {
+    res.send("you are register");
 })
-app.get("/gallery", (req,res) => {
-    res.render("gallery.ejs");
-})
-app.get("/blogs", (req,res) => {
-    res.render("blogs.ejs");
-})
-app.get("/contact-us", (req,res) => {
-    res.render("contactUs.ejs");
-})
+app.post("/register", (req,res) => {
+            let {name , email , contact , city , requirement , msg } = req.body;
+            let newClient =  new Client({name , email , contact , city , requirement , msg});
+            newClient.save().then((res) => {console.log(res)}).catch((err) => {console.log(err)});
+            res.redirect("/");       
+    })
 
-app.listen(3000, () => {
-    console.log("app is listening on port 3000");
+app.listen(8080, () => {
+    console.log("app is listening on port 8080");
 })
 
 
 
 
 
+// app.get("/about-us", (req,res) => {
+//     res.render("aboutUs");
+// })
+// app.get("/gallery", (req,res) => {
+//     res.render("gallery.ejs");
+// })
+// app.get("/blogs", (req,res) => {
+//     res.render("blogs.ejs");
+// })
+// app.get("/contact-us", (req,res) => {
+//     res.render("contactUs.ejs");
+// })
---
+++ models/client.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// schema
+const clientSchema = mongoose.Schema({
+    name :{
+        type : String,
+        required : true
+    }
+   ,
+    email : {
+        type : String,
+        required : true,
+    } ,
+    contact: {
+        type : Number,
+        required : true,
+    } ,
+    requirement : {
+        type : String,
+        required : true,
+    } ,
+    
+    msg : {
+        type : String,
+        required : true,
+    } ,
+})
+
+// modle
+ module.exports = mongoose.model("Client" , clientSchema);
+
