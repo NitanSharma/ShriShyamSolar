bf4e52054c6b0a5d905580774fe3404b6757064c
Nitin <sharmanitan938@gmail.com>
2025-03-23 21:02:13 +0530

    Adding Submit Success using flash

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const Client = require("./models/client.js")
+const session = require("express-session");
+const flash = require("connect-flash");
 
 app.set("view engine" , "ejs");// for views folder
 app.set("views", path.join(__dirname, "views"));
@@ -15,12 +17,25 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));// for public folder
 app.use(express.urlencoded({extended : true}));
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));// express session using
+app.use(flash());
+
 let dbUrl = process.env.ATLASDB_URL;
 // mongodb://127.0.0.1:27017/shreeshyam
 main().then(() => {console.log("Connected to DB")}).catch(err => console.log(err));
 async function main() {
     await mongoose.connect(dbUrl);
 }
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+})
 app.get("/" , (req,res) => {
     res.render("home.ejs");
 })
@@ -32,6 +47,7 @@ app.post("/register" , (req,res) => {
     let {name , contact , city , requirement ,bill, email,msg } = req.body;
     let newClient =  new Client({name ,contact , city , requirement , bill, email ,msg});
     newClient.save().then((res) => {console.log(res)}).catch((err) => {console.log(err)});
+    req.flash("success" , "Form Submit Successfully");
     res.redirect("/");    
 })
 app.get("/gallery", (req,res) => {
