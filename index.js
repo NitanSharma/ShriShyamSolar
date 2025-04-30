b8a6428a1ceaaee4b0b34fa7cc67602d001f22ae
Nitin <sharmanitan938@gmail.com>
2025-04-30 19:25:17 +0530

    Adding express-validator and vercel.json

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const ejsMate = require("ejs-mate");
 const Client = require("./models/client.js")
 const session = require("express-session");
 const flash = require("connect-flash");
+const {body , validationResult} = require("express-validator");
+
 
 app.set("view engine" , "ejs");// for views folder
 app.set("views", path.join(__dirname, "views"));
@@ -25,7 +27,7 @@ app.use(session({
 app.use(flash());
 
 let dbUrl = process.env.ATLASDB_URL;
-// mongodb://127.0.0.1:27017/shreeshyam
+// let dbUrl =  mongodb://127.0.0.1:27017/shreeshyam
 main().then(() => {console.log("Connected to DB")}).catch(err => console.log(err));
 async function main() {
     await mongoose.connect(dbUrl);
@@ -42,13 +44,35 @@ app.get("/" , (req,res) => {
 app.get("/register" , (req,res) => {
     res.send("hello");
 })
-app.post("/register" , (req,res) => {
+app.post("/register" , 
+    [
+        body('name').notEmpty().withMessage('Name is required'),
+        body('contact').isMobilePhone().withMessage('Valid contact number is required'),
+        body('city').notEmpty().withMessage('City is required'),
+        body('requirement').notEmpty().withMessage('Requirement is required'),
+        body('bill').isNumeric().withMessage('Bill must be a number'),
+        body('email').isEmail().withMessage('Valid email is required'),
+        body('msg').notEmpty().withMessage('Message is required')
+    ]
+    ,(req,res) => {
+
+    try {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors : errors.array() });
+    }
     console.log(req.body);
     let {name , contact , city , requirement ,bill, email,msg } = req.body;
     let newClient =  new Client({name ,contact , city , requirement , bill, email ,msg});
     newClient.save().then((res) => {console.log(res)}).catch((err) => {console.log(err)});
-    req.flash("success" , "Form Submit Successfully");
-    res.redirect("/");    
+    req.flash("success" , "Form Submit Successfully.");
+    res.redirect("/"); 
+        
+    } catch (error) {
+        req.flash("error" , "Please enter valid data.");
+    }
+
+      
 })
 app.get("/gallery", (req,res) => {
     res.render("gallery.ejs");
