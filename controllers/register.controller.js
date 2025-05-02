b4669aac2297e6d3ed1e063575118f6522970fbf
Nitin <sharmanitan938@gmail.com>
2025-05-02 18:46:54 +0530

    DB URL Add but not working

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,15 +1,15 @@
 const {validationResult} = require("express-validator");
 const Client = require("../models/client")
 
-module.exports.registerController = (req,res) => {
+module.exports.registerController = async (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors : errors.array() });
     }
-    console.log(req.body);
+    // console.log(req.body);
     let {name , contact , city , requirement ,bill, email,msg } = req.body;
     let newClient =  new Client({name ,contact , city , requirement , bill, email ,msg});
-    newClient.save().then((res) => {console.log(res)}).catch((err) => {console.log(err)});
+    await newClient.save();
     req.flash("success" , "Form Submit Successfully.");
     res.redirect("/");  
 }
--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ let dbUrl = process.env.ATLASDB_URL;
 connectToDB().then(() => {console.log("Connected to DB")}).catch(err => console.log("Connection Failed\n", err));
 
 async function connectToDB() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/shreeshyam');
+    await mongoose.connect(dbUrl);
 }
 
 module.exports = connectToDB;
